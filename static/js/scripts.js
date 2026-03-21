518a1fcbffd8e6216181a61b83b8b1653aaa1054
Cauan Nascimento <caunas1337@proton.me>
2026-03-21 18:22:24 -0300

    refactor: CSS e JS movidos para o diretório de arquivos estáticos

---
+++ static/js/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgers = document.querySelectorAll('.navbar-burger');
+
+    burgers.forEach(el => {
+        el.addEventListener('click', () => {
+            const target = document.getElementById(el.dataset.target);
+            el.classList.toggle('is-active');
+            target.classList.toggle('is-active');
+        });
+    });
+});
